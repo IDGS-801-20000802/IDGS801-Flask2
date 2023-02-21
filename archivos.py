f8ef8e1216288bbde7ee2eca168a7bd38e9ac66a
Erick Lozano <jesselozano395@gmail.com>
2023-02-20 23:11:08 -0600

    Primer commit flask2 20/02/2023

---
+++ archivos.py
@@ -0,0 +1,8 @@
+
+
+file=open('alumnos.txt', 'r')
+nombres=file.readline()
+print(nombres)
+file.close()
+for item in nombres:
+    print(item, end='')
---
+++ forms.py
@@ -0,0 +1,18 @@
+from wtforms import Form 
+from flask_wtf import FlaskForm 
+from wtforms import StringField, SubmitField, FieldList, FormField, SelectField 
+from wtforms.fields import EmailField, TextAreaField, RadioField, PasswordField
+from wtforms import validators
+
+class UserForm(Form):
+    nombre = StringField('Matricula', [
+        validators.DataRequired(message='El campo es requerido'),
+        validators.length(min=4, max=10, message='long de campo 4 min and 5 max')
+    ])
+    nombre = StringField('Nombre', [
+    validators.DataRequired(message="El campo es requerido")
+    ])
+    
+    apaterno = StringField('Apaterno')
+    amaterno = StringField('Amaterno') 
+    email = EmailField('Correo')
